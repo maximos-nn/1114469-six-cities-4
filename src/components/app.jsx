5af1ec9f1bef61b66c15c267728b689063f611d0
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2020-06-10 20:22:42 +0300

    Личный проект: начинаем программировать

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "./main.jsx";
+
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {placesCount} = props;
+
+  return (
+    <Main placesCount={placesCount} />
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app.jsx";
+
+const PLACES_COUNT = 312;
+
+ReactDOM.render(
+    <App placesCount={PLACES_COUNT} />,
+    document.querySelector(`#root`)
+);
