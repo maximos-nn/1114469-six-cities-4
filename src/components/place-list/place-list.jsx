92427079e98fa48c30c6296bf4e82e6cebfcbd1d
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2020-07-29 18:59:28 +0300

    Преобразование в функциональный компонент

--- src/components/place-list/place-list.jsx
+++ src/components/place-list/place-list.jsx
@@ -1,29 +1,27 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import {placeListType} from "../prop-types";
 import PlaceCard from "../place-card/place-card.jsx";
 
-class PlaceList extends PureComponent {
-  render() {
-    const {places, listClass} = this.props;
-    const forwardedProps = Object.assign({}, this.props);
-    delete forwardedProps.places;
-    delete forwardedProps.listClass;
-    return (
-      <div className={`${listClass} places__list`}>
-        {places.map((card) => {
-          return (
-            <PlaceCard
-              key={card.id}
-              card={card}
-              {...forwardedProps}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+const PlaceList = (props) => {
+  const {places, listClass} = props;
+  const forwardedProps = Object.assign({}, props);
+  delete forwardedProps.places;
+  delete forwardedProps.listClass;
+  return (
+    <div className={`${listClass} places__list`}>
+      {places.map((card) => {
+        return (
+          <PlaceCard
+            key={card.id}
+            card={card}
+            {...forwardedProps}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 PlaceList.propTypes = {
   places: placeListType,
