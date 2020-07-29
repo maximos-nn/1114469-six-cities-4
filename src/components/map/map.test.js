f02853678d300bf977f90cf3e77237f9d0c35899
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2020-07-29 19:20:03 +0300

    Корректировка теста карты

--- src/components/map/map.test.js
+++ src/components/map/map.test.js
@@ -1,101 +1,27 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import Map from "./map.jsx";
-import {PlaceType} from "../../const";
-import {getLocations} from "../../utils";
 
 const MAP_CLASS_NAME = `cities__map`;
 
-const offers = [
+const markers = [
   {
-    id: 12,
-    title: `Beautiful & luxurious apartment at great location`,
-    type: PlaceType.APARTMENT,
-    picture: `img/apartment-01.jpg`,
-    price: 235,
-    rating: 4,
-    isBookmarked: false,
-    isPremium: true,
-    guests: 4,
-    bedrooms: 3,
-    description: `Description.`,
-    amenities: [
-      `Cabel TV`,
-      `Fridge`
-    ],
-    host: {
-      name: `Angelina`,
-      avatar: `img/avatar-angelina.jpg`,
-      isSuper: true
-    },
-    photos: [
-      `img/studio-01.jpg`,
-      `img/apartment-01.jpg`
-    ],
     location: [52.3909553943508, 4.85309666406198],
-    city: {name: `Amsterdam`}
+    active: false
   },
   {
-    id: 32,
-    title: `Canal View Prinsengracht`,
-    type: PlaceType.APARTMENT,
-    picture: `img/apartment-02.jpg`,
-    price: 157,
-    rating: 3.3,
-    isBookmarked: false,
-    isPremium: false,
-    guests: 4,
-    bedrooms: 3,
-    description: `Description.`,
-    amenities: [
-      `Cabel TV`,
-      `Fridge`
-    ],
-    host: {
-      name: `Angelina`,
-      avatar: `img/avatar-angelina.jpg`,
-      isSuper: true
-    },
-    photos: [
-      `img/studio-01.jpg`,
-      `img/apartment-01.jpg`
-    ],
     location: [52.369553943508, 4.85309666406198],
-    city: {name: `Amsterdam`}
+    active: false
   },
   {
-    id: 42,
-    title: `Nice, cozy, warm big bed apartment`,
-    type: PlaceType.APARTMENT,
-    picture: `img/apartment-03.jpg`,
-    price: 234,
-    rating: 5,
-    isBookmarked: true,
-    isPremium: true,
-    guests: 4,
-    bedrooms: 3,
-    description: `Description.`,
-    amenities: [
-      `Cabel TV`,
-      `Fridge`
-    ],
-    host: {
-      name: `Angelina`,
-      avatar: `img/avatar-angelina.jpg`,
-      isSuper: true
-    },
-    photos: [
-      `img/studio-01.jpg`,
-      `img/apartment-01.jpg`
-    ],
     location: [52.3909553943508, 4.929309666406198],
-    city: {name: `Amsterdam`}
+    active: false
   }
 ];
 
 it(`Map should render correctly`, () => {
   const tree = renderer.create(
-      <Map mapClass={MAP_CLASS_NAME} markers={getLocations(offers, null)} />,
+      <Map mapClass={MAP_CLASS_NAME} markers={markers} />,
       {createNodeMock: () => document.createElement(`div`)}
   ).toJSON();
   expect(tree).toMatchSnapshot();
